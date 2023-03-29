532444b6ab292350012001a3149c18773e59e34d
Allan Barcelos <allan@barcelos.dev>
2023-03-28 20:41:21 -0400

    fix register

--- www/js/register.js
+++ www/js/register.js
@@ -3,8 +3,17 @@ const api = `//${window.location.hostname}:3000`;
 
 const form = document.querySelector("#register-form");
 
+const registerButton = document.getElementById("register-button");
+const loadingButton = document.getElementById("loading-button");
+
+loadingButton.style.display = "none";
+registerButton.style.display = "block";
+
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
+
+  loadingButton.style.display = "block";
+  registerButton.style.display = "none";
 
   const formData = new FormData(form);
   const name = formData.get("name");
@@ -18,11 +27,15 @@ form.addEventListener("submit", async (event) => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ name, email, password }),
+  }).catch(() => {
+    loadingButton.style.display = "none";
+    registerButton.style.display = "block";
   });
 
-  if (!response.ok) {
-    const errorText = await response.text();
-    alert(errorText);
+  if (!response?.ok) {
+    const errorText = await response?.text();
+    if (errorText) alert(errorText);
+    else alert("System Failure");
     return;
   }
 
